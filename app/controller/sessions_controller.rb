d1422ef1ec6f676c79b0bb42e28d981a4bf62d2c
Laura Berge <lauraeberge@gmail.com>
2021-02-17 16:19:45 -0600

    stub out sessions routes

--- app/controller/sessions_controller.rb
+++ app/controller/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     post "/login" do
         redirect_if_logged_in
         user = User.find_by(email: params["user"]["email"])
-        if user && user.authenticate(params[params["user"]["password"])
+        if user && user.authenticate(params["user"]["password"])
             session["user_id"] = user.id
             redirect "/"
         else
