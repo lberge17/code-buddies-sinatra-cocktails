a0f993f4397594cf8b96b3594c6b04109fdcaf4a
Laura Berge <lauraeberge@gmail.com>
2021-02-17 16:19:02 -0600

    add forbidden route and flash

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -5,10 +5,15 @@ class ApplicationController < Sinatra::Base
         set :views, 'app/views'
         enable :sessions
         set :session_secret, ENV['SESSION_SECRET']
+        use Rack::Flash
     end
 
     get '/' do
         erb :home
+    end
+
+    get '/forbidden' do
+        erb :forbidden
     end
 
     helpers do
